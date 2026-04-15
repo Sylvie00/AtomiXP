2e399d797ea9e26c8bdc67fc2215a5f6d9f33367
May <may@MeowbookM3P-2.local>
2026-04-15 17:32:04 -0500

    rewrite xfce-winxp-tc install script (thanks oomf) and add deps

--- files/scripts/xfceconf.sh
+++ files/scripts/xfceconf.sh
@@ -2,14 +2,12 @@
 set -euo pipefail
 
 TEMP_DIR=$(mktemp -d)
+trap 'rm -rf "$TEMP_DIR"' EXIT
+
 git clone --depth 1 https://github.com/rozniak/xfce-winxp-tc.git "$TEMP_DIR"
-cd "$TEMP_DIR"
-cd packaging
+cd "$TEMP_DIR/packaging"
 chmod +x buildall.sh chkdeps.sh
+
 ./buildall.sh
-BUILD_OUTPUT_DIR="./xptc"
-if [ -d "$BUILD_OUTPUT_DIR" ]; then
-    find "$BUILD_OUTPUT_DIR" -name "*.rpm" -exec rpm2cpio {} | cpio -idmv -D / \;
-fi
-cd /
-rm -rf "$TEMP_DIR"
+
+find ./xptc -name "*.rpm" -print0 | xargs -0 dnf install -y --nogpgcheck
