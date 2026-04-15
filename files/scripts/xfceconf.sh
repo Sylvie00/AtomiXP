bf4252de2ad22a19540f91037f3425cdde86100b
Sylvie00 <taylorkellogg540@gmail.com>
2026-04-14 23:35:41 -0500

    first attempt at adding xfce-winxp-tc

--- files/scripts/xfceconf.sh
+++ files/scripts/xfceconf.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Make sure the labwc autostart is executable
-if [ -f /etc/skel/.config/labwc/autostart ]; then
-    chmod +x /etc/skel/.config/labwc/autostart
+TEMP_DIR=$(mktemp -d)
+git clone --depth 1 https://github.com/rozniak/xfce-winxp-tc.git "$TEMP_DIR"
+cd "$TEMP_DIR"
+cd packaging
+chmod +x buildall.sh chkdeps.sh
+./buildall.sh
+BUILD_OUTPUT_DIR="./xptc"
+if [ -d "$BUILD_OUTPUT_DIR" ]; then
+    find "$BUILD_OUTPUT_DIR" -name "*.rpm" -exec rpm2cpio {} | cpio -idmv -D / \;
 fi
+cd /
+rm -rf "$TEMP_DIR"
