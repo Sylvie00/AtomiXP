6ed4eb71f24dee27cf65f78c0d0edea4dd3545a7
Sylvie00 <taylorkellogg540@gmail.com>
2026-04-16 20:04:16 -0500

    use latest available version of xfce-winxp-tp

--- files/scripts/theme.sh
+++ files/scripts/theme.sh
@@ -3,13 +3,10 @@ set -xueo pipefail
 
 dnf5 install -y $(cat packages/xfce-winxp-tc/build-deps.txt)
 
-XFCE_WINXP_TC_VERSION="1a2f8d5b1e43bafaa29d95718274f6080ee0908b"
-
 mkdir -p /tmp/xfce-winxp-tc
 cd /tmp/xfce-winxp-tc
 git clone https://github.com/rozniak/xfce-winxp-tc.git
 cd xfce-winxp-tc
-git checkout $XFCE_WINXP_TC_VERSION
 bash packaging/buildall.sh
 
 rpm-ostree install xptc/*/rpm/std/x86_64/fre/wintc-*.rpm
