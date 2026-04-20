09a55f32cdb1fc3354d6daaaf3d9da76257d854e
Sylvie00 <taylorkellogg540@gmail.com>
2026-04-19 21:16:11 -0500

    fix theme install script for existing users

--- files/system/usr/bin/applytheme.sh
+++ files/system/usr/bin/applytheme.sh
@@ -6,7 +6,7 @@ apply_theme() {
     local home=$(getent passwd "$user" | cut -d: -f6)
     
     # skip if already configured
-    [ -f "$home/.config/xfce4" ] && return
+    [ -f "$home/.config/atomixp-configured" ] && return
     
     mkdir -p "$home/.config/xfce4/xfconf/xfce-perchannel-xml"
     
@@ -15,20 +15,12 @@ apply_theme() {
         "$home/.config/xfce4/xfconf/xfce-perchannel-xml/"
     cp /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfwm4.xml \
         "$home/.config/xfce4/xfconf/xfce-perchannel-xml/"
-    cp /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml \
-        "$home/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml"
-    cp /etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml \
-        "$home/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml"
-    cp "/etc/skel/.config/autostart/WinTC Desktop.desktop" \
-        "$home/.config/autostart/WinTC Desktop.desktop"
-    cp "/etc/skel/.config/autostart/WinTC Taskband.desktop" \
-        "$home/.config/autostart/WinTC Taskband.desktop"
-    
+
     chown -R "$user:$user" "$home/.config/xfce4"
-    
+    touch "$home/.config/atomixp-configured"
+    chown "$user:$user" "$home/.config/atomixp-configured"
 }
 
-# get all human users (uid 1000+)
 while IFS=: read -r user _ uid _; do
     [ "$uid" -ge 1000 ] && apply_theme "$user"
 done < /etc/passwd
