ebb33ac92b10d49b4f0ecd347dc8f9e263e48414
Sylvie00 <taylorkellogg540@gmail.com>
2026-04-24 16:44:41 -0500

    nevermind lol just layer it

--- files/scripts/opentabletdrv.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-mkdir -p /tmp/otdrv
-cd /tmp/otdrv
-curl -LO https://github.com/OpenTabletDriver/OpenTabletDriver/releases/download/v0.6.6.2/opentabletdriver-0.6.6.2-1.x86_64.rpm
-
-rpm-ostree install opentabletdriver*.rpm
