cf7539923d2ce0ef6008f671730f6b46d3c7252a
Sylvie00 <taylorkellogg540@gmail.com>
2026-05-09 16:07:16 -0500

    Pull redmondxp from github rather than just shoving it in this repo

--- files/scripts/theme.sh
+++ files/scripts/theme.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -xueo pipefail
 
+#install latest xfce-wintc-xp
 dnf5 install -y $(cat packages/xfce-winxp-tc/build-deps.txt)
 
 mkdir -p /tmp/xfce-winxp-tc
@@ -12,3 +13,8 @@ bash packaging/buildall.sh
 rpm-ostree install xptc/*/rpm/std/x86_64/fre/wintc-*.rpm
 
 plymouth-set-default-theme bootvid
+
+#install RedmondXP as an alternative theme
+git clone https://github.com/phush0/redmondxp.git /tmp/redmondxp
+mkdir -p /usr/share/themes
+cp -r /tmp/redmondxp/RedmondXP /usr/share/themes/
